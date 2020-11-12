1d1590fc9eec700835c44d128f52c534b2386d01
Jonas Molina Ramirez <jonas.molinaramirez@stud.tu-darmstadt.de>
2020-11-12 23:51:33 +0100

    Add reworked version
    Two selection areas available:
    - select which tag to add
    - select which tags to display
    
    Notes are displayed if at least one of the tags in a notes
    tag set is marked visible.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,78 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const TagSelector = ({ tags, setTags, isTagSelected, setTagSelected }) => {
+
+  const selectTag = tag => {
+    setTags(
+      tags.map(t => t.id === tag.id ?
+        setTagSelected(t) : t))
+  }
+  return (
+    <div>
+      <ul>
+        {tags.map(t =>
+          <li
+            key={t.id}
+            style={isTagSelected(t) ? { background: "yellow" } : {}}
+            onClick={e => selectTag(t)}
+          >{t.value}</li>)}
+      </ul>
+    </div>
+  )
+}
+
+const App = () => {
+  const [tags, setTags] = useState([
+    { id: 1, value: "Ticket 1", visible: false, selected: false },
+    { id: 2, value: "Ticket 2", visible: false, selected: false }])
+
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState("")
+
+  const addNote = e => {
+    e.preventDefault()
+    const newId = notes.length === 0 ? 0 : Math.max(...notes.map(n => n.id)) + 1
+    setNotes(notes.concat({ value: newNote, id: newId, tagIds: tags.filter(t => t.selected).map(t => t.id) }))
+    setNewNote("")
+  }
+
+  const handleNoteChange = (event) => {
+    setNewNote(event.target.value)
+  }
+  return (
+    <div>
+      <h1>Test</h1>
+      <h2>Select</h2>
+      <TagSelector
+        tags={tags}
+        setTags={setTags}
+        isTagSelected={t => t.selected}
+        setTagSelected={t => { return { ...t, selected: !t.selected } }}
+      />
+      <h2>Visible</h2>
+      <TagSelector
+        tags={tags}
+        setTags={setTags}
+        isTagSelected={t => t.visible}
+        setTagSelected={t => { return { ...t, visible: !t.visible } }}
+      />
+      <button onClick={e => setTags(tags.map(t => { return { ...t, visible: false } }))}>Clear Selection</button>
+
+      <h2>Notes</h2>
+      <form>
+        <textarea value={newNote} onChange={handleNoteChange} rows="4" cols="50" />
+        <button onClick={addNote}>Add Note</button>
+      </form>
+      <ul>
+        {(tags.filter(t => t.visible).length === 0 ? notes :
+          notes
+            .filter(n => n.tagIds.some(t => tags.find(e => e.id === t).visible)))
+          .map(n => <li key={n.id}>[{n.tagIds.map(id=>tags.find(t=>t.id === id).value).join()}] ID: {n.id} {n.value}</li>)}
+      </ul>
+    </div>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
@@ -10,8 +80,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
